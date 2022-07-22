333dd8a8c1580c936f47d59e5629cb83dc59f39b
Dale Short <short.dale@gmail.com>
2022-07-22 12:59:04 -0400

    working project CRUD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,272 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-
+import "./App.css";
+import React, { useReducer, useEffect, useState } from "react";
+import axios from "axios";
+import ListGroup from "react-bootstrap/ListGroup";
+import Card from "react-bootstrap/Card";
+import "./bootstrap.min.css";
+import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/esm/FloatingLabel";
+import Modal from "react-bootstrap/Modal";
 
 function App() {
+  function refreshProjectList() {
+    //update project list
+    axios
+      .get("http://127.0.0.1:8000/project/projects/")
+      .then((response) => {
+        console.log(response.data);
+        setState({ projects: response.data, isLoadingProjectList: false });
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("issue loading project list");
+        setState({ isLoadingProjectList: false });
+      });
+  }
+
+  const handleProjectFormSubmit = (event) => {
+    event.preventDefault();
+    setState({ isLoadingProjectDetail: true });
+    if (state.activeProject == "new") {
+      //create new project
+      let copy_projectDetail = { ...state.projectDetail };
+      delete copy_projectDetail.id;
+      axios
+        .post("http://127.0.0.1:8000/project/projects/", copy_projectDetail)
+        .then((response) => {
+          console.log(response.data);
+          setState({
+            isLoadingProjectDetail: false,
+            isLoadingProjectList: true,
+          });
+          refreshProjectList();
+        })
+        .catch((err) => {
+          console.log(err);
+          alert("issue updating project details");
+          setState({ isLoadingProjectDetail: false });
+        });
+    }
+    //update existing project
+    else {
+      axios
+        .put(
+          "http://127.0.0.1:8000/project/projects/" +
+            state.activeProject.toString() +
+            "/",
+          state.projectDetail
+        )
+        .then((response) => {
+          console.log(response.data);
+          setState({
+            isLoadingProjectDetail: false,
+            isLoadingProjectList: true,
+          });
+          refreshProjectList();
+        })
+        .catch((err) => {
+          console.log(err);
+          alert("issue updating project details");
+          setState({ isLoadingProjectDetail: false });
+        });
+    }
+  };
+
+  //handles changes to project form state
+  const handleProjectFormChange = (event) => {
+    console.log(event);
+    let copy_projectDetail = state.projectDetail;
+    copy_projectDetail[event.target.name.toString()] = event.target.value;
+
+    setState({ projectDetail: copy_projectDetail });
+  };
+
+  function renderProjectForm() {
+    //this is breaking because form data isnt being set right?
+    if (state.projectDetail !== null) {
+      return (
+        <div>
+          <Form onSubmit={handleProjectFormSubmit}>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Title"
+              className="mb-3"
+            >
+              <Form.Control
+                name="title"
+                onChange={handleProjectFormChange}
+                onSelect={handleProjectFormChange}
+                value={state.projectDetail.title || ""}
+              />
+            </FloatingLabel>
+
+            <Button type="submit">Submit</Button>
+            <Button variant="danger" onClick={handleProjectModalShow}>
+              Delete
+            </Button>
+          </Form>
+          {renderDeleteModal()}
+        </div>
+      );
+    }
+  }
+
+  const [showProjectModal, setShowProjectModal] = useState(false);
+
+  const handleProjectModalClose = () => setShowProjectModal(false);
+  const handleProjectModalShow = () => setShowProjectModal(true);
+  const handleProjectModalDelete = () => {
+    setState({ isLoadingProjectDetail: true });
+    axios
+      .delete(
+        "http://127.0.0.1:8000/project/projects/" +
+          state.activeProject.toString() +
+          "/",
+        state.projectDetail
+      )
+      .then((response) => {
+        console.log(response.data);
+        setState({
+          isLoadingProjectDetail: false,
+          isLoadingProjectList: true,
+          activeProject: null,
+          projectDetail: null,
+        });
+        //update project list
+        axios
+          .get("http://127.0.0.1:8000/project/projects/")
+          .then((response) => {
+            console.log(response.data);
+            setState({ projects: response.data, isLoadingProjectList: false });
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("issue loading project list");
+            setState({ isLoadingProjectList: false });
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("issue updating project details");
+        setState({ isLoadingProjectDetail: false });
+      });
+  };
+
+  function renderDeleteModal() {
+    return (
+      <Modal show={showProjectModal} onHide={handleProjectModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Delete Project</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {"Are you sure you want to delete project " +
+            state.projectDetail.title +
+            "?"}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleProjectModalClose}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={handleProjectModalDelete}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  //general state management
+  const [state, setState] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      projects: null,
+      isLoadingProjectList: true,
+      activeProject: null,
+      isLoadingProjectDetail: false,
+      projectDetail: null,
+    }
+  );
+
+  //load projects list on page load
+  useEffect(() => {
+    refreshProjectList();
+  }, []);
+
+  useEffect(() => {
+    if (state.activeProject !== null) {
+      console.log("active project changed to:", state.activeProject);
+
+      if (state.activeProject == "new") {
+        console.log("new project");
+        setState({
+          projectDetail: { id: "new", title: "" },
+        });
+      } else {
+        setState({ isLoadingProjectDetail: true });
+        axios
+          .get(
+            "http://127.0.0.1:8000/project/projects/" +
+              state.activeProject.toString()
+          )
+          .then((response) => {
+            console.log(response.data);
+            setState({
+              projectDetail: response.data,
+              isLoadingProjectDetail: false,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("issue loading");
+            setState({ isLoadingProjectDetail: false });
+          });
+      }
+    }
+  }, [state.activeProject]);
+
+  function setCurrentProject(project_id) {
+    setState({ activeProject: project_id });
+  }
+
+  function renderProjectList() {
+    return (
+      <Card style={{ width: "18rem" }}>
+        <ListGroup variant="flush" as="ul">
+          {state.projects
+            ? state.projects.map((x, i) => {
+                return (
+                  <ListGroupItem
+                    action
+                    active={state.activeProject == x.id}
+                    onClick={() => {
+                      setCurrentProject(x.id);
+                    }}
+                  >
+                    {x.title}{" "}
+                  </ListGroupItem>
+                );
+              })
+            : null}
+          <ListGroupItem
+            action
+            onClick={() => {
+              setCurrentProject("new");
+            }}
+          >
+            *start a new project*
+          </ListGroupItem>
+        </ListGroup>
+      </Card>
+    );
+  }
+
+
   return (
     <div className="App">
-
+      {renderProjectList()}
+      {renderProjectForm()}
     </div>
   );
 }
--- fulcrum/fulcrum/settings.py
+++ fulcrum/fulcrum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-53hyyp9(!%up3!2g(@(t##op6c_#lx1ji^d!+8^e@x)fl3%y^2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'http://127.0.0.1:8000', 'http://localhost:3000']
 
 
 # Application definition
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'project',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
